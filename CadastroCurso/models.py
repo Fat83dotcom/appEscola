1bce9577aa5c651a5ca595fbbe377e4c4e1305eb
fat83dotcom <fernando.quimic@gmail.com>
2022-09-10 17:59:42 -0300

    Substituição das declarações de cores para var() no CSS

--- CadastroCurso/models.py
+++ CadastroCurso/models.py
@@ -1,7 +1,15 @@
 from cadastros.models import Curso
 from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 
 class FormCurso(ModelForm):
     class Meta:
-        pass
+        model = Curso
+        fields = ['cod_c', 'nome_c', 'cod_dep']
+        # exclude = ()
+        labels = {
+            'cod_c': _('Código do Curso:'),
+            'nome_c': _('Nome do Curso:'),
+            'cod_dep': _('Código do Departamento:'),
+        }
--- CadastroCurso/views.py
+++ CadastroCurso/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from .models import FormCurso
 
 
 @login_required(redirect_field_name='login-system')
 def cadastraCurso(request):
-    return render(request, 'cadastraCurso/cadastraCurso.html')
+    data = datetime.today()
+    if request.method != 'POST':
+        formularioCurso = FormCurso(request.POST)
+        return render(request, 'cadastraCurso/cadastraCurso.html', {
+            'data': data,
+            'formCurso': formularioCurso
+        })
 
 
 @login_required(redirect_field_name='login-system')
