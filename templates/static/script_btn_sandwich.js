702e8ee4eba41cdbdc4da0b2054a05e84142a457
fat83dotcom <fernando.quimic@gmail.com>
2022-09-11 22:19:37 -0300

    Criação de um menu sandwich para o site pc ou mobile

---
+++ templates/static/script_btn_sandwich.js
@@ -0,0 +1,8 @@
+const openNav = () =>{
+    document.getElementById("drop").style.display = "flex"
+    document.getElementById('navBtn').style.display = "none"
+} 
+const closeNav = () =>{
+    document.getElementById("drop").style.display = "none"
+    document.getElementById('navBtn').style.display = "flex"
+} 
