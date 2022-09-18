308e368b21e59bcdc2b3a7d70d54dd4b1509a5db
fat83dotcom <fernando.quimic@gmail.com>
2022-09-18 18:07:32 -0300

    Desenvolvimento da pagina de consultas simples dos alunos

--- ConsultasGerais/views.py
+++ ConsultasGerais/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from cadastros import models
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def consultaAluno(request):
-    # cruza a consulta da tabela Aluno com a chave estrangeira endereco!
-    dadosAluno = models.Aluno.objects.prefetch_related('endereco')
-    messages.success(request, 'Consulta realizada com sucesso !')
+    if request.method == 'GET':
+        # cruza a consulta da tabela Aluno com a chave estrangeira endereco!
+        dadosAluno = models.Aluno.objects.prefetch_related('endereco')
+        paginacao = Paginator(dadosAluno, 20)
+        pagina = request.GET.get('p')
+        dadosAluno = paginacao.get_page(pagina)
 
-    return render(request, 'ConsultasGerais/consultaAluno.html', {
-        'dadosJuntados': dadosAluno,
-    })
+        estatAlunosQtdT = models.Aluno.objects.count()
+        estatAlunosQtdP = len(dadosAluno)
+        messages.success(request, 'Consulta realizada com sucesso !')
+        return render(request, 'ConsultasGerais/consultaAluno.html', {
+            'dadosJuntados': dadosAluno,
+            'estatAlunoT': estatAlunosQtdT,
+            'estatAlunoP': estatAlunosQtdP
+        })
 
 
 def consultaCurso(request):
