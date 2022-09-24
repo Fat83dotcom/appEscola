0aab51b872d2ff5087557c0099fef2754f61028c
fat83dotcom <fernando.quimic@gmail.com>
2022-09-24 12:22:22 -0300

    Refatoração da paginação

--- ConsultasAvancadas/views.py
+++ ConsultasAvancadas/views.py
@@ -15,48 +15,43 @@ def formatadorDatas(dataEntrada):
         return None
 
 
+def paginacao(request, query, nPaginas):
+    return Paginator(query, nPaginas).get_page(request.GET.get('p'))
+
+
 @login_required(redirect_field_name='login-system')
 def consultas(request):
-    if request.method != 'GET':
-        formulario = FormConsulta(request.GET)
+    formulario = FormConsulta(request.GET)
+    if formulario.is_valid():
+        try:
+            pesquisa = request.GET.get('pesquisa')
+            print(pesquisa)
+            if pesquisa == '' or pesquisa is None:
+                mensagens(request, 'war', 'Digite um nome, sobrenome, data de nascimento, cpf ou parte deles.')
+                return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
+                    'form': formulario,
+                })
+            resultadoPesquisa = Aluno.objects.prefetch_related('endereco').order_by('nome_aluno').filter(
+                Q(cpf__icontains=pesquisa) | Q(nome_aluno__icontains=pesquisa) |
+                Q(sobrenome_aluno__icontains=pesquisa) | Q(dt_nasc=formatadorDatas(pesquisa))
+            )
+            if len(resultadoPesquisa) == 0:
+                mensagens(request, 'war', f'Não foram encotrados dados relacionados a {pesquisa}')
+                return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
+                    'form': formulario,
+                })
+            resultadoPesquisa = paginacao(request, resultadoPesquisa, 10)
+            mensagens(request, 'suc', 'Pesquisa realizada com Sucesso')
+            return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
+                'form': formulario,
+                'resp': resultadoPesquisa,
+            })
+        except Exception as erro:
+            mensagens(request, 'err', erro)
+            return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
+                'form': formulario,
+            })
+    else:
         return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
             'form': formulario,
         })
-    else:
-        formulario = FormConsulta(request.GET)
-        if formulario.is_valid():
-            try:
-                pesquisa = request.GET.get('pesquisa')
-                print(pesquisa)
-                if pesquisa == '':
-                    mensagens(request, 'war', 'Digite um nome, sobrenome, data de nascimento, cpf ou parte deles.')
-                    return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
-                        'form': formulario,
-                    })
-                resultadoPesquisa = Aluno.objects.prefetch_related('endereco').order_by('nome_aluno').filter(
-                    Q(cpf__icontains=pesquisa) | Q(nome_aluno__icontains=pesquisa) |
-                    Q(sobrenome_aluno__icontains=pesquisa) | Q(dt_nasc=formatadorDatas(pesquisa))
-                )
-                if len(resultadoPesquisa) == 0:
-                    mensagens(request, 'war', f'Não foram encotrados dados relacionados a {pesquisa}')
-                    return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
-                        'form': formulario,
-                    })
-                paginator = Paginator(resultadoPesquisa, 10)
-                pagina = request.GET.get('p')
-                resultadoPesquisa = paginator.get_page(pagina)
-                mensagens(request, 'suc', 'Pesquisa realizada com Sucesso')
-                return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
-                    'form': formulario,
-                    'resp': resultadoPesquisa,
-                })
-            except Exception as erro:
-                mensagens(request, 'err', erro)
-                return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
-                    'form': formulario,
-                })
-        else:
-            return render(request, 'ConsultasAvancadas/consultaAvancada.html', {
-                'form': formulario,
-                # 'resp': resposta,
-            })
