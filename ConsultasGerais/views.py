05d171a375037459d76bde6507ee4dfeaf8217cf
fat83dotcom <fernando.quimic@gmail.com>
2022-09-29 18:13:31 -0300

    Refatoração da consulta SQL dos detalhes dos alunos

--- ConsultasGerais/views.py
+++ ConsultasGerais/views.py
@@ -48,11 +48,12 @@ def consultaGeralAlunos(modelo):
 def detalhesEscolaresAlunos(cpf) -> tuple | None:
     with connection.cursor() as cursor:
         cursor.execute(
-            "SELECT nome_aluno, sobrenome_aluno, dt_nasc, cadastros_curso.cod_c, nome_c, "
-            "numero_matricula, dt_matricula FROM cadastros_aluno, cadastros_matriculaaluno, "
-            "cadastros_curso WHERE cadastros_aluno.cpf=%s AND "
-            "cadastros_aluno.cpf=cadastros_matriculaaluno.cpf AND "
-            "cadastros_matriculaaluno.cod_c=cadastros_curso.cod_c", (cpf,)
+            'SELECT nome_aluno, sobrenome_aluno, dt_nasc, cadastros_curso.cod_c, nome_c, '
+            'numero_matricula, dt_matricula FROM cadastros_aluno INNER JOIN cadastros_matriculaaluno '
+            'ON(cadastros_aluno.cpf=cadastros_matriculaaluno.cpf) INNER JOIN cadastros_curso '
+            'ON(cadastros_matriculaaluno.cod_c=cadastros_curso.cod_c)WHERE cadastros_aluno.cpf=%s '
+            'GROUP BY nome_aluno, sobrenome_aluno, dt_nasc, cadastros_curso.cod_c, nome_c, '
+            'numero_matricula, dt_matricula ORDER BY nome_aluno', (cpf,)
         )
         return cursor.fetchone()
 
@@ -61,15 +62,14 @@ def detalhesEscolaresAlunos(cpf) -> tuple | None:
 def detalhesEscolaresCurso(codCurso):
     with connection.cursor() as cursor:
         cursor.execute(
-            'select cadastros_curso.cod_c, nome_c, cod_d, nome_disciplina, cadastros_professor.matricula_prof, '
-            'nome_prof, sobrenome_prof from cadastros_curso inner join cadastros_matriculaaluno '
-            'on(cadastros_curso.cod_c=cadastros_matriculaaluno.cod_c) inner join cadastros_grade '
-            'on(cadastros_matriculaaluno.cod_c=cadastros_grade.cod_curso) inner join cadastros_disciplina '
-            'on(cadastros_grade.cod_disciplina=cadastros_disciplina.cod_d) inner join cadastros_professor '
-            'on(cadastros_disciplina.matricula_prof=cadastros_professor.matricula_prof) where cadastros_matriculaaluno.cod_c=%s'
-            'group by cadastros_curso.cod_c, nome_c, cod_d, nome_disciplina, cadastros_professor.matricula_prof, '
-            'sobrenome_prof, nome_prof order by nome_disciplina;',
-            (codCurso,)
+            'SELECT cadastros_curso.cod_c, nome_c, cod_d, nome_disciplina, cadastros_professor.matricula_prof, '
+            'nome_prof, sobrenome_prof FROM cadastros_curso INNER JOIN cadastros_matriculaaluno '
+            'ON(cadastros_curso.cod_c=cadastros_matriculaaluno.cod_c) INNER JOIN cadastros_grade '
+            'ON(cadastros_matriculaaluno.cod_c=cadastros_grade.cod_curso) INNER JOIN cadastros_disciplina '
+            'ON(cadastros_grade.cod_disciplina=cadastros_disciplina.cod_d) INNER JOIN cadastros_professor '
+            'ON(cadastros_disciplina.matricula_prof=cadastros_professor.matricula_prof) WHERE cadastros_matriculaaluno.cod_c=%s'
+            'GROUP BY cadastros_curso.cod_c, nome_c, cod_d, nome_disciplina, cadastros_professor.matricula_prof, '
+            'sobrenome_prof, nome_prof ORDER BY nome_prof', (codCurso,)
         )
         return cursor.fetchall()
 
@@ -109,8 +109,6 @@ def consultaAluno(request):
 @login_required(redirect_field_name='login-system')
 def consultaCurso(request):
     resultadoPesquisa = models.Curso.objects.prefetch_related('cod_dep')
-    nAlunosCursos = models.MatriculaAluno.objects.select_related('cod_c')
-    print(nAlunosCursos)
     nCursos = len(resultadoPesquisa)
     contexto = {
         'result': resultadoPesquisa,
