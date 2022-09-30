78e4f55c4973eb6280bad16064571d3fae2a3077
fat83dotcom <fernando.quimic@gmail.com>
2022-09-30 19:37:21 -0300

    Refatoração de partes do codigo para padronização

--- ConsultasGerais/views.py
+++ ConsultasGerais/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from funcoesUsoGeral import paginacao, mensagens, mensagensMaisUsadas, log
 
 
-def idadeMedia() -> float:
+def consultaIdadeMedia() -> float:
     with connection.cursor() as cursor:
         try:
             cursor.execute(
@@ -16,14 +16,14 @@ def idadeMedia() -> float:
             return 0
 
 
-def menor18() -> int:
+def consultaMenor18() -> int:
     with connection.cursor() as cursor:
         cursor.execute(
             'SELECT COUNT(*) FROM cadastros_aluno WHERE EXTRACT(YEAR FROM AGE(dt_nasc))<18')
         return int(cursor.fetchone()[0])
 
 
-def entre18E30() -> int:
+def consultaEntre18E30() -> int:
     with connection.cursor() as cursor:
         cursor.execute(
             'SELECT COUNT(*) FROM cadastros_aluno WHERE EXTRACT(YEAR FROM AGE(dt_nasc)) BETWEEN 18 AND 30'
@@ -31,7 +31,7 @@ def entre18E30() -> int:
         return int(cursor.fetchone()[0])
 
 
-def acima30() -> int:
+def consultaAcima30() -> int:
     with connection.cursor() as cursor:
         cursor.execute(
             'SELECT COUNT(*) FROM cadastros_aluno WHERE EXTRACT(YEAR FROM AGE(dt_nasc))>30')
@@ -45,7 +45,7 @@ def consultaGeralAlunos(modelo):
 
 
 @log
-def detalhesEscolaresAlunos(cpf) -> tuple | None:
+def consultaDetalhesEscolaresAlunos(cpf) -> tuple | None:
     with connection.cursor() as cursor:
         cursor.execute(
             'SELECT nome_aluno, sobrenome_aluno, dt_nasc, cadastros_curso.cod_c, nome_c, '
@@ -59,7 +59,7 @@ def detalhesEscolaresAlunos(cpf) -> tuple | None:
 
 
 @log
-def detalhesEscolaresCurso(codCurso) -> list[tuple]:
+def consultaDetalhesEscolaresCurso(codCurso) -> list[tuple]:
     with connection.cursor() as cursor:
         cursor.execute(
             'SELECT cadastros_curso.cod_c, nome_c, cod_grade, cod_d, nome_disciplina, cadastros_professor.matricula_prof, '
@@ -74,7 +74,7 @@ def detalhesEscolaresCurso(codCurso) -> list[tuple]:
         return cursor.fetchall()
 
 
-def materiasCurso(codCurso) -> list[tuple]:
+def consultaMateriasCurso(codCurso) -> list[tuple]:
     with connection.cursor() as cursor:
         cursor.execute(
             "SELECT nome_disciplina FROM cadastros_grade, cadastros_disciplina,"
@@ -84,21 +84,21 @@ def materiasCurso(codCurso) -> list[tuple]:
         return cursor.fetchall()
 
 
-def estatisticaCurso(codC) -> tuple | None:
+def consultaEstatisticaCurso(codCurso) -> tuple | None:
     with connection.cursor() as cursor:
         cursor.execute(
-            'SELECT COUNT(cpf) FROM cadastros_matriculaaluno WHERE cod_c=%s', (codC, )
+            'SELECT COUNT(cpf) FROM cadastros_matriculaaluno WHERE cod_c=%s', (codCurso, )
         )
-        nAlunos = cursor.fetchone()
+        nAlunos = cursor.fetchone()[0]
         cursor.execute(
-            'SELECT COUNT(cod_disciplina) FROM cadastros_grade WHERE cod_curso=%s', (codC, )
+            'SELECT COUNT(cod_disciplina) FROM cadastros_grade WHERE cod_curso=%s', (codCurso, )
         )
-        nDisciplinas = cursor.fetchone()
+        nDisciplinas = cursor.fetchone()[0]
         cursor.execute(
             'SELECT nome_dep FROM cadastros_curso INNER JOIN cadastros_departamento '
-            'ON(cadastros_curso.cod_dep=cadastros_departamento.cod_dep) WHERE cod_c=%s', (codC, )
+            'ON(cadastros_curso.cod_dep=cadastros_departamento.cod_dep) WHERE cod_c=%s', (codCurso, )
         )
-        departamento = cursor.fetchone()
+        departamento = cursor.fetchone()[0]
         return (nAlunos, nDisciplinas, departamento)
 
 
@@ -115,10 +115,10 @@ def consultaAluno(request):
             'dadosJuntados': dadosAluno,
             'estatAlunoT': estatAlunosQtdT,
             'estatAlunoP': estatAlunosQtdP,
-            'idadeMedia': idadeMedia(),
-            'menor18': menor18(),
-            'entre18e30': entre18E30(),
-            'acima30': acima30(),
+            'idadeMedia': consultaIdadeMedia(),
+            'menor18': consultaMenor18(),
+            'entre18e30': consultaEntre18E30(),
+            'acima30': consultaAcima30(),
             'temp': round(log, 3),
             'nResult': estatAlunosQtdT
         })
@@ -148,9 +148,9 @@ def detalhesAluno(request):
     else:
         try:
             cpf = request.GET.get('cpf')
-            dadoAluno, log = detalhesEscolaresAlunos(cpf)
+            dadoAluno, log = consultaDetalhesEscolaresAlunos(cpf)
             codCurso = dadoAluno[3]
-            grade = materiasCurso(codCurso)
+            grade = consultaMateriasCurso(codCurso)
             grade = (materia[0] for materia in grade)
             mensagens(request, 'suc', mensagensMaisUsadas['consSuc'])
             return render(request, 'ConsultasGerais/detalhesAlunos.html', {
@@ -171,23 +171,23 @@ def detalhesCurso(request):
     else:
         try:
             codCurso = request.GET.get('codC')
-            resultadoPesquisa, log = detalhesEscolaresCurso(codCurso)
+            resultadoPesquisa, log = consultaDetalhesEscolaresCurso(codCurso)
             codDisciplina, nomeDisciplina, codGrade = resultadoPesquisa[0][0],\
                 resultadoPesquisa[0][1], resultadoPesquisa[0][2]
             nEstat = len(resultadoPesquisa)
             resultadoPesquisa = (tupla[3::] for tupla in resultadoPesquisa)
-            nAlunos, nDisciplinas, departamento = estatisticaCurso(codCurso)
+            nAlunos, nDisciplinas, departamento = consultaEstatisticaCurso(codCurso)
             mensagens(request, 'suc', mensagensMaisUsadas['consSuc'])
             return render(request, 'ConsultasGerais/detalhesCursos.html', {
                 'dadosC': resultadoPesquisa,
-                'temp': round(log, 3),
-                'eResult': nEstat,
                 'codC': codDisciplina,
                 'nomeC': nomeDisciplina,
                 'codG': codGrade,
-                'estatAluno': nAlunos[0],
-                'estatDisci': nDisciplinas[0],
-                'estatDep': departamento[0],
+                'temp': round(log, 3),
+                'eResult': nEstat,
+                'estatAluno': nAlunos,
+                'estatDisci': nDisciplinas,
+                'estatDep': departamento,
             })
         except Exception as erro:
             mensagens(request, 'err', f'{mensagensMaisUsadas["consFal"]}... {erro}')
