a18e98e1b89fb319e08f9e1af066787cb4ae2cba
fat83dotcom <fernando.quimic@gmail.com>
2022-10-04 19:59:45 -0300

    Atualização do cadastro dos professores - novos campos

--- CadastroProfessor/models.py
+++ CadastroProfessor/models.py
@@ -7,16 +7,26 @@ from django import forms
 class FormProfessor(ModelForm):
     class Meta:
         model = Professor
-        fields = ['nome_prof', 'sobrenome_prof',  ]
+        fields = ['nome_prof', 'sobrenome_prof', 'n_contrato', 'data_contrato', 'departamento']
         labels = {
             'nome_prof': _('Nome Professor:'),
-            'sobrenome_prof': _('Sobrenome Professor:')
+            'sobrenome_prof': _('Sobrenome Professor:'),
+            'n_contrato': _('Número contrato'),
+            'data_contrato': _('Data contrato'),
+
         }
         widgets = {
             'nome_prof': forms.TextInput(attrs={
-                'placeholder': 'Digite o nome do Professor',
+                'placeholder': 'Digite o nome do Professor.',
             }),
             'sobrenome_prof': forms.TextInput(attrs={
-                'placeholder': 'Digite o sobrenome do professor'
+                'placeholder': 'Digite o sobrenome do professor.',
+            }),
+            'n_contrato': forms.TextInput(attrs={
+                'placeholder': 'Número do Contrato.',
+            }),
+            'data_contrato': forms.DateInput(attrs={
+                'placeholder': 'DD/MM/AAAA',
             })
+
         }
